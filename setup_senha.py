00da9e0aa8296a56861e2cf67ea8c11b5cd4ae0e
luizajuridicomgmultas-design <luizajuridicomgmultas@gmail.com>
2025-10-22 17:45:45 -0300

    Change input prompts in setup_senha.py
    Updated input prompts for CPF and password.

--- setup_senha.py
+++ setup_senha.py
@@ -1,8 +1,8 @@
 from crypto_config import criptografar
 
 def main():
-    cpf = input("Digite seu CPF (só números): ")
-    senha = input("Digite sua senha Gov.br: ")
+    cpf = input("02187146618: ")
+    senha = input("Fopoqen1@: ")
     senha_cript = criptografar(senha)
     print(f"Coloque este valor no Railway como GOVBR_PASSWORD_ENCRYPTED:\n{senha_cript}")
 
